20bb9709c66922ea652b5d37c45684820ab99e7c
Akshay Deo <99003245+akde-odoo@users.noreply.github.com>
2022-12-27 10:24:11 +0530

    Add files via upload

---
+++ custom_module/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    #  Information
+    'name': 'Odoo Projects',
+    'version': '15.0',
+    'summary': '',
+    'description': '',
+    'category': '',
+
+    # Author
+    'author': 'Odoo PS',
+    'website': 'https://www.odoo.com/',
+    'license': 'LGPL-3',
+
+    # Dependency
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+    ],
+
+    # Other
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
---
+++ custom_module/models/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+
+class Test(models.Model):
+    _name = 'test'
